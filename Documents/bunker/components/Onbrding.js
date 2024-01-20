93eb4ba6c30a032afee81056c93e821051a904eb
internet_trash <naiknischal92@gmail.com>
2024-01-20 13:27:33 +0530

    first commit

---
+++ Documents/bunker/components/Onbrding.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default  Onbrding = () => {
+  return (
+    <View>
+      <Text>Onbrding lawda te lassan</Text>
+    </View>
+  )
+}
+
+ 
